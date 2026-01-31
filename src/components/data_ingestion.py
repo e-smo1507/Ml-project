f80740cbb855f8a5ae5f10738a8c23a9ac2ac71a
e-smo1507 <esmoliguni@gmail.com>
2026-01-31 13:16:14 +0530

    data ing

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -15,11 +15,34 @@ class DataIngestionConfig:
                                     
 class DataIngestion:
   def __init__(self):
-    self.ingestion_config = DataIngestion()
+    self.ingestion_config = DataIngestionConfig()
 
   def initiate_data_ingestion(self):
     logging.info("Entered the data ingestion method or component")
     try:
       df = pd.read_csv('notebook\data\stud.csv')
-    except:
-      pass
+      logging.info('Read the dataset as Datafram')
+
+      os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok = True)
+
+      df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
+
+      logging.info("Train test split initiated")
+      train_set,test_set= train_test_split(df,test_size=0.2,random_dtate=42)
+
+      train_set.to_csv(self.ingestion_config.train_data_path, index=False,header=True)
+
+      test_set.to_csv(self.ingestion_config.test_data_path,index=False, header=True)
+     
+      logging.info("Ingestion of the data iss completed")
+
+      return(
+        self.ingestion_config.train_data_path,
+        self.ingestion_config.test_data_path
+      )
+    except Exception as e:
+      raise CustomException(e,sys)
+
+if __name__=="__main__":
+  obj=DataIngestion()
+  obj.initiate_data_ingestion()
