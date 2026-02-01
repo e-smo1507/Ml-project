c4f321d845145f230b0669250d7c20391307aec5
e-smo1507 <esmoliguni@gmail.com>
2026-02-01 19:38:47 +0530

    pipeline

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,6 +6,8 @@ import  pandas as pd
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from src.components.data_transform import DataTransformation
+from src.components.data_transform import DataTransformationConfig
 
 @dataclass
 class DataIngestionConfig:
@@ -28,7 +30,7 @@ class DataIngestion:
       df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
 
       logging.info("Train test split initiated")
-      train_set,test_set= train_test_split(df,test_size=0.2,random_dtate=42)
+      train_set,test_set= train_test_split(df,test_size=0.2,random_state=42)
 
       train_set.to_csv(self.ingestion_config.train_data_path, index=False,header=True)
 
@@ -45,4 +47,7 @@ class DataIngestion:
 
 if __name__=="__main__":
   obj=DataIngestion()
-  obj.initiate_data_ingestion()
+  train_data,test_data = obj.initiate_data_ingestion()
+
+  data_transformation= DataTransformation()
+  data_transformation.initiate_data_trsnformation(train_data,test_data)
